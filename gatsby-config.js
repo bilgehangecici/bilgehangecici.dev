d9cc4af4210eb4e00b05f13f0e4e3aedd6453d54
Bilgehan Gecici <bilgehangecici@gmail.com>
2021-08-22 11:51:17 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,8 +25,6 @@ module.exports = {
         pluginConfig: {
           // Puts tracking script in the head instead of the body
           head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
         },
       },
     },
